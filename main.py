9a2939050c57cec9dfbbe81e40ea17b511b700fb
Ian Nytes <ian.nytes@gmail.com>
2021-02-26 12:48:41 -0600

    fixed gitignore issue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import gspread
+from pprint import PrettyPrinter as pprint
+
 
 gc = gspread.service_account(filename='sheets_auth.json')
 sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/17aL0l7sOI_S1Xdd9gG1xsYCU72PagJz5kG2qswIXtHw')
