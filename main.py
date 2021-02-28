d5f297be53a6e5eed0a711d21a2388472a36a0a4
Ian Nytes <ian.nytes@gmail.com>
2021-02-28 17:37:23 -0600

    added datetime and fixed sheets connection issues.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import gspread
 from pprint import PrettyPrinter as pprint
+import datetime
 
 
-gc = gspread.service_account(filename='sheets_auth.json')
-sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/17aL0l7sOI_S1Xdd9gG1xsYCU72PagJz5kG2qswIXtHw')
+gc = gspread.service_account(filename="sheets_auth.json")
+sh = gc.open_by_key('17aL0l7sOI_S1Xdd9gG1xsYCU72PagJz5kG2qswIXtHw')
+current_date = datetime.datetime.now()
+
 
 hours_sheet = sh.sheet1
-
+values = hours_sheet.row_values(2)
 
 #Check to see if new hours have been added.
+print(values)
 
 
 #Check to see what billable week is by pulling date in a block of 2 weeks.
