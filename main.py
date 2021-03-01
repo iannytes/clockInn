3b5be49f81e11390a6f221bbc379420b8658ae2f
Ian Nytes <ian.nytes@gmail.com>
2021-03-01 00:41:34 -0600

    added list for entered dates and a fucntion to add those dates to a list.

--- main.py
+++ main.py
@@ -2,18 +2,40 @@ import gspread
 from pprint import PrettyPrinter as pprint
 import datetime
 
+from pyasn1.type.univ import Null
+
 
 gc = gspread.service_account(filename="sheets_auth.json")
 sh = gc.open_by_key('17aL0l7sOI_S1Xdd9gG1xsYCU72PagJz5kG2qswIXtHw')
 current_date = datetime.datetime.now()
 
+# creates list of dates in program
+datetimes = []
+
+#finds earliesy date in list
+
+#variable for 2 week pay period interval
+pay_period_delta = datetime.timedelta(days=14)
+
 
 hours_sheet = sh.sheet1
 values = hours_sheet.row_values(2)
 
-#Check to see if new hours have been added.
-print(values)
 
+#Check all dates in sheet and add them to list (date col is 4)
+def add_dates(col) :
+    loop = 1
+    for rows in hours_sheet.col_values(col):
+        val = hours_sheet.cell(loop, col).value
+        #!!! find out how to not return an empty value on the end of your list.
+        if len(val) > 2:
+            loop = loop + 1
+            val = hours_sheet.cell(loop, col).value
+            datetimes.append(val)
+
+add_dates(4)
+
+print(max(datetimes))
 
 #Check to see what billable week is by pulling date in a block of 2 weeks.
 
