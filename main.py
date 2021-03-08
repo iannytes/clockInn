ac433d3ea7904fbdf7ab4b932053b2a2ceb52fc7
Ian Nytes <ian.nytes@gmail.com>
2021-03-08 00:32:19 -0600

    restored working version

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
 import gspread
 import datetime
-
-
-
-
 gc = gspread.service_account(filename="sheets_auth.json")
 sh = gc.open_by_key('17aL0l7sOI_S1Xdd9gG1xsYCU72PagJz5kG2qswIXtHw')
 current_date = datetime.datetime.now()
-
 # creates list of dates in program
 datetimes = []
 super_email = []
 employee_names = []
-
-worked_hours{
-
-}
-
 hours_sheet = sh.sheet1
-
-
-
 #Check all dates in sheet and add them to list (date col is 4)
 def make_list(col, list) :
     loop = 2
@@ -31,21 +18,24 @@ def make_list(col, list) :
             if len(val) > 0 :
             #make sure the list has no repeats.
                 if val not in list :
-                 worked_hours[val] = hours_sheet.cell(loop, 2).value
-                    loop = loop + 1 
+                     list.append(val)
+                     loop = loop + 1 
         except:
-            return False
+             print('exception')
+             return False
 
- 
+
 def get_pay_period():
     start_date = datetime.datetime.strptime(min(datetimes), "%m/%d/%y")
     end_date = start_date + datetime.timedelta(days=14)
     print(start_date)
     print(end_date)
-
             
 #creates list for super email names of employees and dates worked.        
 make_list(4, datetimes)
 make_list(1, employee_names)
 make_list(6, super_email)
 get_pay_period()
+print(datetimes)
+print(employee_names)
+print(super_email)
