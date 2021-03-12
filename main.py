cdcc5a35184784dafc0dcbc026190ca33ee0c369
Ian Nytes <ian.nytes@gmail.com>
2021-03-12 17:32:03 -0600

    added email loop

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import datetime
 gc = gspread.service_account(filename="sheets_auth.json")
 sh = gc.open_by_key('17aL0l7sOI_S1Xdd9gG1xsYCU72PagJz5kG2qswIXtHw')
 current_date = datetime.datetime.now()
-# creates list of dates in program
-datetimes = []
-super_email = []
-employee_names = []
-hour_match = []
+# creates lists of data in program 
+datetimes = [] #days that have been worked
+super_email = [] #supervisor emails
+employee_names = [] #names of Employess
+hour_match = [] #How many hours thos employees have worked
+
+
 hours_sheet = sh.sheet1
-#Check all dates in sheet and add them to list (date col is 4)
+#Check data in sheet and make a list from it.
 def make_list(col, list) :
     loop = 2
     for rows in hours_sheet.col_values(col):
@@ -25,15 +27,15 @@ def make_list(col, list) :
              print('exception')
              return False
 
-
+#gets two weeks from the earliest submitted date of work
 def get_pay_period():
     start_date = datetime.datetime.strptime(min(datetimes), "%m/%d/%y")
     end_date = start_date + datetime.timedelta(days=14)
     print(start_date)
     print(end_date)
             
-
-def get_hour(name,):
+#calculates hours worked for a given name in the list
+def get_hour(name):
     loop = 0
     global hours_worked
     hours_worked = 0
@@ -42,8 +44,10 @@ def get_hour(name,):
         if name == hours_sheet.cell(loop, 1).value:
             hours_worked = int(hours_sheet.cell(loop, 2).value) + hours_worked
 
+def email_alert():
+    print('goes to email')
            
-#creates list for super email names of employees and dates worked.        
+#creates list for super email, names of employees, and dates worked.        
 make_list(4, datetimes)
 make_list(1, employee_names)
 make_list(6, super_email)
@@ -52,13 +56,22 @@ print(datetimes)
 print(employee_names)
 print(super_email)
 
-
+#matches hours to names
 for name in employee_names:
     get_hour(name)
     hour_match.append(hours_worked)
 
-    
+print(hour_match)
+loop = 0
+for name in employee_names:
+    permission_to_email = input('Would you like to submit -' + str(hour_match[loop]) + '- hours worked for ' + str(employee_names[loop]) + '? [y/n]')
+    if permission_to_email.upper == 'Y' :
+        email_alert(loop)
+        loop = loop + 1 
+    else :
+        permission_to_email = 'null'
+        loop = loop + 1
 
-print(employee_names, hour_match)
+
             
         
